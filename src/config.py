588863baa678d91380a4f5b138ed3d67090194e4
Knowbein <132909661+Knowbein@users.noreply.github.com>
2023-09-29 15:25:34 +0300

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    token: str = '6144455818:AAFgRZtFKM9PmPZ06X6MjojunQQ85c-7hCA'
+    admin_ids: int = '123456789, 987654321'
+    pay_token: str = '632593626:TEST:sandbox_i37040672644'
+    item_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmrrMKdvOCcXcmWZe3mJ5ymnqajf5CtfJoPQ&usqp=CAU"
+    item_url2 = "https://cdn-icons-png.flaticon.com/512/5504/5504229.png"
---
+++ src/handlers/__init__.py
@@ -0,0 +1,4 @@
+from .user import dp
+from .cart import dp
+
+__all__ = [dp]
